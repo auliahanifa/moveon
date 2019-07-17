9e4ffad3a0005e33c0b657b60135f3be4a35dd8d
estiros <estirosmanasmkn1@gmail.com>
2019-07-17 22:57:52 +0700

    navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="background-color:#1f4f77; padding-top:20px; padding-bottom:5px">
+    <center>
+      <p style="color:white; font-size:12px">Copyright © Bimbingan Alumni UI</p>
+    </center>
+  </div>
+</template>
+<script>
+export default {
+  name: "Footer"
+};
+</script>
