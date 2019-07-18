c64a09b6ab2fc8bce1735b36e13069898b70ffb7
Hanifa <hanifalia91@gmail.com>
2019-07-18 17:42:53 +0700

    detail lelang

---
+++ src/components/DetailBaranglelang.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="detail-barang">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <b-card></b-card>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<script>
+export default {
+    name:'detail-lelang'
+}
+</script>
+<style scoped>
+
+</style>
