68f438f3ce9e8792fa938a0a08e9a5150320b47d
Hanifa <hanifalia91@gmail.com>
2019-07-29 14:52:19 +0700

    barang lelang galangdana pengguna

--- src/components/GalangDana.vue
+++ src/components/GalangDana.vue
@@ -47,7 +47,7 @@ export default {
   },
     created() {
     axios
-      .get(`http://localhost:8000/api/galangdana`)
+      .get(`http://127.0.0.1:8000/api/galangdana`)
       .then(response => {
         // JSON responses are automatically parsed.
         this.galangdana = response.data.galangdana;
