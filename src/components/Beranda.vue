f08fdfdd9fd7bf51cbdd6f2fb26354b771d1a7c8
estiros <estirosmanasmkn1@gmail.com>
2019-08-04 01:56:58 +0700

    beranda

--- src/components/Beranda.vue
+++ src/components/Beranda.vue
@@ -1,153 +1,180 @@
 <template>
   <div class="Beranda">
-    <b-container class="header-section">
-      <b-row>
-        <h3>{{ header_text1 }}</h3>
-      </b-row>
+    <div class="header-section">
+      <h3>{{ header_text1 }}</h3>
       <h4>{{ header_text1_a }}</h4>
       <h5>{{ header_text2 }}</h5>
       <router-link to="/donasi_barang">
         <b-button variant="outline" class="btn_app">{{ btn_donasibarang }}</b-button>
       </router-link>
       <div class="batasan"></div>
-    </b-container>
-
-    <center>
-      <b-container class="body-cerita">
+    </div>
+    <b-container>
+      <div class="body-cerita">
         <b-row>
-          <b-col sm="12" md="6" lg="6">
+          <b-col sm="12" md="7" lg="8">
             <h3>{{ judul_section }}</h3>
             <h5>{{ deskripsi_section }}</h5>
-            <router-link to="/formgalangdanaa">
-              <b-button variant="primary" class="btn_app2">{{ btn_mulaigd }}</b-button>
-            </router-link>
+            <center>
+              <router-link to="/formgalangdanaa">
+                <b-button class="btn-orange-stgh">{{ btn_mulaigd }}</b-button>
+              </router-link>
+            </center>
           </b-col>
-          <b-col sm="12" md="6" lg="6">
-            <b-card
-              overlay
-              img-src="@/assets/img/charity.jpg"
-              img-alt="Card Image"
-              text-variant="white"
-              title="Nama Galang Dana"
-              v-bind:sub-title="dana_target"
-            >
-              <b-card-text>
-                {{ nama_fundraiser }}
-                <br />
-                <center>
-                  <router-link to="/donasi">
-                    <b-button>Lihat semua</b-button>
-                  </router-link>
-                </center>
-              </b-card-text>
-            </b-card>
+          <b-col lg="4" md="5" sm="12">
+            <center v-if="galang_beasiswa != null">
+              <b-card
+                :img-src="'http://admin.donasimoveon.com' + galang_beasiswa.path_photo"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="text-align:left; "
+                class="mb-2 img-card"
+              >
+                <p style="font-size:16px; font-weight:bold">{{galang_beasiswa.judul}}</p>
+                <p style="font-size:16px;">
+                  Target Dana
+                  <span
+                    style="font-weight:bold;"
+                  >{{galang_beasiswa.target_dana | currency}}</span>
+                </p>
+                <b-progress
+                  :value="parseInt(galang_beasiswa.dana_terkini)"
+                  :max="parseInt(galang_beasiswa.target_dana)"
+                  class="mb-3"
+                  variant="warning"
+                  style="border-radius:10px;"
+                ></b-progress>
+                <div class="row">
+                  <div class="col-md-8">
+                    <p style="font-size:14px;">
+                      <span style="font-weight:bold;">{{ galang_beasiswa.dana_terkini | currency }}</span> terkumpul
+                    </p>
+                  </div>
+                  <div class="col-md-4">
+                    <p style="font-size:14px; text-align: right">
+                      <span style="font-weight:bold; ">{{ sisa_hari }}</span> Hari Lagi
+                    </p>
+                  </div>
+                </div>
+                <router-link to="/donasi">
+                  <b-button class="btn-orange">Lihat Semua</b-button>
+                </router-link>
+              </b-card>
+            </center>
+            <center v-else>
+              <h6>Saat ini Galang Dana Beasiswa Tidak Tersedia</h6>
+            </center>
           </b-col>
         </b-row>
-      </b-container>
-    </center>
-    <hr />
+      </div>
 
-    <b-container class="body-baranglelang">
-      <br />
-      <br />
+      <hr />
 
-      <b-row>
-        <h3>Ikuti lelang di moveon lalu salurkan uang anda untuk galang dana yang menjadi destinasi bantuan anda</h3>
-      </b-row>
-      <br />
-
-      <br />
-
-      <center>
-        <router-link to="/lelang">
-          <b-button>Lihat semua</b-button>
-        </router-link>
-      </center>
-    </b-container>
-
-    <hr />
-
-    <b-container class="body-team">
-      <center>
-        <h3 style="border-bottom: 5px solid red; width:230px; padding:10px;">Meet Our Team</h3>
+      <div class="body-baranglelang">
         <br />
         <br />
+
         <b-row>
-          <b-col class="col-sm-12 col-md-6 col-lg-6">
-            <img src="@/assets/img/hanifa.png" alt="ava" />
-            <h6>
-              Hanifa Aulia
-              <br />Progressive Web App Implementation
-            </h6>
-            <!-- <p>hanifalia91@gmail.com</p> -->
-            <h6>
-              <a
-                href="https://www.linkedin.com/in/hanifa-aulia-473322166/"
-                target="__blank"
-                class="card-link"
-              >
-                <img src="@/assets/img/linkedin.png" alt="linkdin" />
-              </a>
-              <a href="https://www.instagram.com/heigun/" target="__blank" class="card-link">
-                <img src="@/assets/img/igteam.png" alt="ig" />
-              </a>
-            </h6>
-          </b-col>
-          <b-col class="col-sm-12 col-md-6 col-lg-6">
-            <img src="@/assets/img/esti.png" alt="ava" />
-            <h6>
-              Esti Rosmana
-              <br />Multi Factors Evaluation Process Implementation
-            </h6>
-            <!-- <p>estirosmanasmkn1@gmail.com</p> -->
-            <h6>
-              <a
-                href="https://www.linkedin.com/in/esti-rosmana-b60140129/"
-                target="__blank"
-                class="card-link"
-              >
-                <img src="@/assets/img/linkedin.png" alt="linkdin" />
-              </a>
-              <a href="https://www.instagram.com/estiros/" target="__blank" class="card-link">
-                <img src="@/assets/img/igteam.png" alt="ig" />
-              </a>
-            </h6>
-          </b-col>
+          <h3>Ikuti lelang di moveon lalu salurkan uang anda untuk galang dana yang menjadi destinasi bantuan anda</h3>
         </b-row>
-      </center>
+        <br />
+
+        <br />
+
+        <center>
+          <router-link to="/lelang">
+            <b-button>Lihat semua</b-button>
+          </router-link>
+        </center>
+
+        <hr />
+        <div class="body-team">
+          <center>
+            <h3 style="border-bottom: 5px solid red; width:230px; ">Meet Our Team</h3>
+            <br />
+            <br />
+            <b-row>
+              <b-col class="col-sm-12 col-md-6 col-lg-6">
+                <img src="@/assets/img/hanifa.png" alt="ava" />
+                <h6>
+                  Hanifa Aulia
+                  <br />Progressive Web App Implementation
+                </h6>
+                <!-- <p>hanifalia91@gmail.com</p> -->
+                <h6>
+                  <a
+                    href="https://www.linkedin.com/in/hanifa-aulia-473322166/"
+                    target="__blank"
+                    class="card-link"
+                  >
+                    <img src="@/assets/img/linkedin.png" alt="linkdin" />
+                  </a>
+                  <a href="https://www.instagram.com/heigun/" target="__blank" class="card-link">
+                    <img src="@/assets/img/igteam.png" alt="ig" />
+                  </a>
+                </h6>
+              </b-col>
+              <b-col class="col-sm-12 col-md-6 col-lg-6">
+                <img src="@/assets/img/esti.png" alt="ava" />
+                <h6>
+                  Esti Rosmana
+                  <br />Multi Factors Evaluation Process Implementation
+                </h6>
+                <!-- <p>estirosmanasmkn1@gmail.com</p> -->
+                <h6>
+                  <a
+                    href="https://www.linkedin.com/in/esti-rosmana-b60140129/"
+                    target="__blank"
+                    class="card-link"
+                  >
+                    <img src="@/assets/img/linkedin.png" alt="linkdin" />
+                  </a>
+                  <a href="https://www.instagram.com/estiros/" target="__blank" class="card-link">
+                    <img src="@/assets/img/igteam.png" alt="ig" />
+                  </a>
+                </h6>
+              </b-col>
+            </b-row>
+          </center>
+        </div>
+
+        <hr />
+
+        <div class="sosmed">
+          <center>
+            <b-row>
+              <b-col sm="12" md="3" lg="3">
+                <img src="@/assets/img/logomuvon.png" alt="logo" />
+              </b-col>
+              <b-col sm="12" md="3" lg="3">
+                <p>Temukan kami di:</p>
+              </b-col>
+              <b-col sm="12" md="6" lg="6">
+                <div class="sosmed-logo">
+                  <img src="@/assets/img/twiter.png" alt="twitter" />
+                  <img src="@/assets/img/fb.png" alt="fb" />
+                  <img src="@/assets/img/iglogo.png" alt="ig" />
+                  <img src="@/assets/img/yutub.png" alt="youtube" />
+                </div>
+              </b-col>
+            </b-row>
+          </center>
+        </div>
+      </div>
     </b-container>
-    <hr />
-
-    <center>
-      <b-container class="sosmed">
-        <b-row>
-          <b-col sm="12" md="3" lg="3">
-            <img src="@/assets/img/logomuvon.png" alt="logo" />
-          </b-col>
-          <b-col sm="12" md="3" lg="3">
-            <p>Temukan kami di:</p>
-          </b-col>
-
-          <b-col sm="12" md="6" lg="6">
-            <div class="sosmed-logo">
-              <img src="@/assets/img/twiter.png" alt="twitter" />
-              <img src="@/assets/img/fb.png" alt="fb" />
-              <img src="@/assets/img/iglogo.png" alt="ig" />
-              <img src="@/assets/img/yutub.png" alt="youtube" />
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </center>
-
     <div style="background-color:#414040; padding-top:10px; padding-bottom:5px">
       <center>
-        <p style="color:white; font-size:12px">Copyright © Moveon Team 2019</p>
+        <router-link
+          to="/"
+          style="color:white; text-decoration: none;"
+        >Copyright © Donasi MoveOn 2019</router-link>
       </center>
     </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "beranda",
   data() {
@@ -168,36 +195,51 @@ export default {
       harga_brg: "Rp. 298,000",
       nama_pemilik: "Anastasya Widyarini",
       dana_target: "",
-      nama_fundraiser: ""
+      nama_fundraiser: "",
+      galang_beasiswa: {},
+      sisa_hari: "",
+      avatar_admin: "",
+      nama_admin: ""
     };
+  },
+  created() {
+    axios
+      .get(`https://admin.donasimoveon.com/api/beasiswa`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.galang_beasiswa = response.data.galang_beasiswa;
+        this.sisa_hari = response.data.sisa_hari;
+        this.avatar_admin = response.data.avatar_admin;
+        this.nama_admin = response.data.nama_admin;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  },
+  filters: {
+    subStr: function(string) {
+      return string.substring(0, 100) + "...";
+    }
   }
 };
 </script>
 <style scoped>
-.Beranda {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif, Quicksand; */
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
-  position: relative;
+hr {
+  border: 3px solid orange;
   width: 100%;
 }
 
-hr {
-  border: 3px solid orange;
-  width: 88%;
-}
-
 .header-section {
-  margin: auto;
+  /* margin: auto; */
   background-image: url("../assets/img/kids.jpg");
   /* background-size: auto; */
-  position: center;
-  background-size: cover;
-  min-width: 100%;
-  min-height: 100%;
+  /* position: center; */
+  /* background-size: cover; */
+  /* min-width: 100%;
+  min-height: 100%; */
   background-repeat: no-repeat;
-  padding: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 50px;
 }
 
 .header-section h3 {
@@ -256,17 +298,9 @@ hr {
 button a {
   color: #ffffff;
 }
-.body-cerita {
-  margin: 50px 0px 0px 0px;
-  background-color: #ffff;
-  min-height: 400px;
-  position: relative;
-}
 
 .body-cerita h3 {
   background-color: yellow;
-  background-size: 200px;
-  /* margin: 6px 30px 28px 10px ; */
   font-style: bold;
   font-family: Quattrocento Sans;
   font-size: 48px;
@@ -286,73 +320,6 @@ button a {
   text-align: left;
 }
 
-/* -- Setting card title --  */
-
-.card-title {
-  margin-top: 20px;
-  font-family: Quicksand;
-  font-weight: 500;
-  font-size: 26px;
-  line-height: 32px;
-  text-align: center;
-  font-style: normal;
-  color: white;
-}
-
-/* .card-text:last-child {
-    margin-bottom: 0;
-    color: #FDFBEB;
-} */
-
-/* -- Setting card img --  */
-
-.card {
-  width: 100%;
-}
-.card-img {
-  width: 100%;
-  height: 320px;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0px;
-  padding: 0.15rem;
-  filter: brightness(72%);
-}
-
-.card-img-top {
-  width: 80%;
-  max-height: 230px;
-  border-top-left-radius: calc(0.25rem - 1px);
-  border-top-right-radius: calc(0.25rem - 1px);
-}
-.card-img-overlay {
-  width: 100%;
-  align-content: center;
-}
-
-.card p {
-  font-size: 18px;
-  font-weight: bold;
-  font-family: Quicksand;
-  text-align: center;
-}
-
-/* -- Setting card subtitle (teks yg ditengah) --  */
-.text-muted {
-  color: #fdfbeb !important;
-  background-color: #2f7196;
-}
-
-.mb-2,
-.my-2 {
-  margin-bottom: 5px !important;
-}
-
-.card-subtitle {
-  margin: 28px;
-}
-
 h6,
 .h6 {
   font-size: 20px;
@@ -361,7 +328,7 @@ h6,
 
 /* -- Setting card img --  */
 
-.body-cerita p {
+/* .body-cerita p {
   margin: 26px;
   font-family: Quicksand;
   font-style: normal;
@@ -370,20 +337,9 @@ h6,
   line-height: 35px;
   color: #ffffff;
   text-align: left;
-}
+} */
 
 /* -- Setting button --  */
-
-.body-cerita .btn-primary {
-  margin: 50px;
-  border-color: #f46158;
-  color: #fff;
-  width: 310px;
-  height: 56px;
-  font-size: 20px;
-  background: #f46158;
-  border-radius: 32px;
-}
 
 .btn-primary:hover {
   color: #fff;
@@ -395,7 +351,6 @@ h6,
   padding: 20px;
   width: 521px;
   height: 58px;
-
   font-family: Quattrocento Sans;
   font-style: normal;
   font-weight: bold;
@@ -403,22 +358,6 @@ h6,
   line-height: 25px;
   display: flex;
   align-items: center;
-
-  color: #2f7196;
-}
-.card-body {
-  /* -ms-flex: 1 1 auto;
-    -webkit-box-flex: 1;
-    flex: 1 1 auto; */
-  padding: 1.25rem;
-}
-
-.card-text {
-  font-family: Quicksand;
-  font-size: 18px;
-}
-
-.card-footer {
   color: #2f7196;
 }
 
@@ -477,10 +416,6 @@ h6,
   width: 44px;
   margin: 1px;
   padding: 2px;
-}
-
-.footer {
-  background-color: #fdfbeb;
 }
 div.card {
   width: 80%;
